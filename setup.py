5a9b3d2902e9ccdf5b5136a72b34d307f373761c
Richard Graham <richardg@sjsoft.com>
2013-01-24 15:25:01 +0200

    Rename to `sentry_jsonmailprocessor`.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ install_requires = [
     ]
 
 setup(
-    name='sentry-jsonmailprocessor',
+    name='sentry_jsonmailprocessor',
     version='0.0.2',
     author='Justin C',
     description='A Sentry extension that forwards Sentry events via SMTP in JSON format.',
